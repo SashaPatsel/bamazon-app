e189c6a082f80bccab1edaba903008f97dfce555
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-02-25 11:05:27 -0800

    new files touched for supervisor functioanlity

--- bamazon-manager.js
+++ bamazon-manager.js
@@ -60,7 +60,7 @@ function viewProds() {
             );
         }
         console.log(table.toString());
-
+        restart()
     });
 }
 
@@ -82,6 +82,7 @@ function viewLowInv() {
         }
         console.log("\nThese are all the items with less than 2000 units left in their inventory:")
         console.log(table.toString());
+        restart()
     })
 }
 
@@ -124,7 +125,7 @@ function addInv() {
 
 
                     viewProds()
-
+                    restart()
                 }
             );
         })
@@ -156,21 +157,29 @@ function addProd() {
                 stock_quantity: answers.sq
             },
             function(err, res) {
-              console.log("\nHere is the updated inventory:")
-              viewProds()
+                console.log("\nHere is the updated inventory:")
+                viewProds()
+                restart()
             }
         );
 
     })
 }
 
-// Create a new Node application called bamazonManager.js. Running this application will:
-// List a set of menu options:
-// View Products for Sale
-// View Low Inventory
-// Add to Inventory
-// Add New Product
-// If a manager selects View Products for Sale, the app should list every available item: the item IDs, names, prices, and quantities.
-// If a manager selects View Low Inventory, then it should list all items with an inventory count lower than five.
-// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+function restart() {
+    inquirer.prompt([{
+        type: "list",
+        name: "continue",
+        choices: ["Yes", "No"],
+        message: "Would you like to do more?\n"
+    }]).then(function(answers) {
+        if (answers.continue === "Yes") {
+            manageProds();
+        } else {
+            console.log("Goodbye!");
+            connection.end();
+        }
+
+    });
+}
+
